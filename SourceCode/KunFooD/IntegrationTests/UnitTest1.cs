2a2bf76f0f1429e03f0baf8faf3bc128523ddc9e
Rusu Cristian <ruscris2@gmail.com>
2017-12-09 16:22:07 +0200

    Generated IntegrationTests project.

--- SourceCode/KunFooD/IntegrationTests/UnitTest1.cs
+++ SourceCode/KunFooD/IntegrationTests/UnitTest1.cs
@@ -1,13 +1,14 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
 
 namespace IntegrationTests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
+            // Test
         }
     }
 }
