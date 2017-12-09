28911f4b65fea38b8b24f960099d38b957ff47e1
TrifanTamara <trifantm@yahoo.com>
2017-12-09 15:19:49 +0200

    fixed swagger

--- SourceCode/KunFooD/WebApp/Startup.cs
+++ SourceCode/KunFooD/WebApp/Startup.cs
@@ -29,13 +29,22 @@ namespace WebApp
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My Todo API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "KunFooD API", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "KunFooD V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
